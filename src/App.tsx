57b87c05ebaf19d6f8d4685ffac4a3929083beb4
Vishist Dura <dvishist27@gmail.com>
2021-07-29 20:52:45 +1000

    implement game

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,100 @@
-import { useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import produce from 'immer'
 const height = 35
-const width = 50
+const width = 55
 
 type cell = 1 | 0
 
+const operations = [
+	[0, 1],
+	[0, -1],
+	[1, 0],
+	[-1, 0],
+	[-1, -1],
+	[-1, 1],
+	[1, -1],
+	[1, 1]
+]
+
+const generateEmptyGrid = () => {
+	const rows: cell[][] = Array(height).fill(Array(width).fill(0))
+	return rows
+}
+
 function App() {
-	const [grid, setGrid] = useState(() => {
-		const rows: cell[][] = Array(height).fill(Array(width).fill(0))
-		return rows
-	})
+	const [grid, setGrid] = useState(generateEmptyGrid)
 
 	const [running, setRunning] = useState(false)
+
+	const runningRef = useRef(running)
+	runningRef.current = running
+
+	const playGame = useCallback(() => {
+		if (!runningRef.current) return
+
+		setGrid(grid => {
+			return produce(grid, newGrid => {
+				for (let i = 0; i < height; i++) {
+					for (let j = 0; j < width; j++) {
+						let neighbours = 0
+						operations.forEach(([x, y]) => {
+							const i2 = i + x
+							const j2 = j + y
+							if (i2 >= 0 && i2 < height && j2 >= 0 && j2 < width) {
+								neighbours += grid[i2][j2]
+							}
+						})
+
+						//apply rules
+						if (neighbours < 2 || neighbours > 3) {
+							newGrid[i][j] = 0
+						} else if (grid[i][j] === 0 && neighbours === 3) {
+							newGrid[i][j] = 1
+						}
+					}
+				}
+			})
+		})
+
+		setTimeout(playGame, 400)
+	}, [])
 
 	return (
 		<>
 			<button
 				onClick={() => {
 					setRunning(!running)
+					if (!running) {
+						runningRef.current = true
+						playGame()
+					}
 				}}
 			>
 				{running ? 'STOP' : 'START'}
 			</button>
+
+			<button
+				onClick={() => {
+					setGrid(() => {
+						const rows = []
+						for (let i = 0; i < height; i++) {
+							rows.push(Array.from(Array(width), () => (Math.random() > 0.8 ? 1 : 0)))
+						}
+						return rows
+					})
+				}}
+			>
+				Random
+			</button>
+
+			<button
+				onClick={() => {
+					setGrid(generateEmptyGrid)
+				}}
+			>
+				Clear
+			</button>
+
 			<div
 				className='App'
 				style={{
@@ -32,13 +105,13 @@ function App() {
 				{grid.map((row, i) =>
 					row.map((cell, j) => (
 						<div
+							key={`${i}:${j}`}
 							style={{
 								width: '18px',
 								height: '18px',
-								backgroundColor: grid[i][j]
-									? '#56638a'
-									: undefined,
-								border: 'solid 0.1px black'
+								backgroundColor: grid[i][j] ? '#56638a' : undefined,
+								border: 'solid 0.1px black',
+								margin: '0'
 							}}
 							onClick={() => {
 								const gridCopy = produce(grid, gridCopy => {
